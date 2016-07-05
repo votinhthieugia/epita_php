31685a9b6e947b5fde9597d88f756de539f5f9ed
Hoang Anh Doan <hoanganhdoan2003@yahoo.com>
2016-07-05 19:11:03 +0200

    Changed rest api call.

--- app/lib/RestApiCall.php
+++ app/lib/RestApiCall.php
@@ -16,32 +16,32 @@ class RestApiCall {
   }
 
   public static function do_post($url, $data) {
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-    $curl = curl_exec($curl);
-    if ($curl === false) {
-      $info = curl_getinfo($curl);
-      curl_close($curl);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $response = curl_exec($ch);
+    if ($response === false) {
+      $info = curl_getinfo($ch);
+      curl_close($ch);
       return false;
     }
-    curl_close($curl);
+    curl_close($ch);
     return true;
   }
 
   public static function do_put($url, $data) {
-    $curl = curl_init($url);
+    $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
     curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-    $response = curl_exec($curl);
+    $response = curl_exec($ch);
     if ($response === false) {
-      $info = curl_getinfo($curl);
-      curl_close($curl);
+      $info = curl_getinfo($ch);
+      curl_close($ch);
       return false;
     }
-    curl_close($curl);
+    curl_close($ch);
     return true;
   }
 
